63e373666171c707604180aa9df6e23d439316f0
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-25 10:51:51 +0800

    completed junit test

--- src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 // Step 1 - Import junit pom from maven repo
 // Step 2 - Set up ServiceTest class in test folder
@@ -72,6 +72,30 @@ public class GradeServiceTest {
         Grade result = gradeService.getGradeById(id);
 
         assertEquals(grade, result);
+
+    }
+
+    @Test
+    public void addGradeTest() {
+        Grade grade = new Grade("Harry", "Potion", "C-");
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        Grade newGrade = new Grade("Dave", "Maths", "A-");
+        gradeService.submitGrade(newGrade);
+        // verify if gradeRepo is called at least once and addGrade is called successfully
+        verify(gradeRepository, times(1)).addGrade(newGrade);
+    }
+
+    @Test
+    public void updateGradeTest() {
+        Grade grade = new Grade("Harry", "Potion", "C-");
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(grade));
+        when(gradeRepository.getGrade(0)).thenReturn(grade);
+
+        grade.setScore("A-");
+        gradeService.submitGrade(grade);
+        verify(gradeRepository, times(1)).updateGrade(grade, 0);
 
     }
 
